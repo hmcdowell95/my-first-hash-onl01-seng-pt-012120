90b686e667daebc94fa36ce964cfa3b121518bcd
Harley McDowell <hmcdowell95@gmail.com>
2020-01-31 19:58:12 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,4 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
   hash = {"name" => "Harley"}
 end
 
@@ -16,8 +15,6 @@ def retrieval
                  }
   shipping_manifest["oil paintings"]
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
 end
 
 def adding
@@ -26,7 +23,6 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
   shipping_manifest["muskets"] = 2 
   shipping_manifest["gun powder"] = 4
 
